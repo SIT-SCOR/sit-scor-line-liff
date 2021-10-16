35fe36dd26c85fb43ab459ad6fcbeaa1798549e0
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-10-16 16:54:57 +0700

    [EDIT] add route for EditInfo #2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Register from './components/Register';
 import Feedback from './components/Feedback';
+import EditInfo from './components/EditInfo';
 
 function App() {
   return (
     <BrowserRouter basename={'/sit-scor-line-liff'}>
       <Switch>
+        <Route exact path="/EditInfo" component={EditInfo} />
         <Route exact path="/Feedback" component={Feedback} />
         <Route exact path="/Register" component={Register} />
         <Route exact path="/" component={Homepage} />

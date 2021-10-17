ccd1d9c8cc736eac97bae6bd33120b8d234a21ef
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-10-17 16:21:28 +0700

    [EDIT] add route for confirm feedback page #2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Homepage from './components/Homepage';
 import Register from './components/Register';
 import Feedback from './components/Feedback';
 import EditInfo from './components/EditInfo';
+import ConfirmFeedback from './components/ConfirmFeedback';
 
 function App() {
   return (
     <BrowserRouter basename={'/sit-scor-line-liff'}>
       <Switch>
+        <Route exact path="/ConfirmFeedback" component={ConfirmFeedback} />
         <Route exact path="/EditInfo" component={EditInfo} />
         <Route exact path="/Feedback" component={Feedback} />
         <Route exact path="/Register" component={Register} />

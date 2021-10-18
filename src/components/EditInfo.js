07407b7075e5130537285c16c48466a7fdf9143a
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-10-18 16:35:40 +0700

    [EDIT] add variable for fetch once time

--- src/components/EditInfo.js
+++ src/components/EditInfo.js
@@ -32,7 +32,7 @@ export default function EditInfo(props) {
             setPassword(info.data.password)
         }
         fetchInfo()
-    })
+    }, [userLineID])
 
     const homepage = () => {
         history.push("/")

e3c88f1f86bb2aee7dacfc1170ee1e1e128b1959
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-10-18 14:47:29 +0700

    [EDIT] change design

--- src/components/EditInfo.js
+++ src/components/EditInfo.js
@@ -1,9 +1,136 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router'
+import Logo from '../images/Logo.png'
+import axios from 'axios';
 
-export default function EditInfo() {
+export default function EditInfo(props) {
+
+    const history = useHistory()
+    const userLineID = props.location.state.userLineID
+    const [lineID, setLineID] = useState("")
+    const [title, setTitle] = useState("")
+    const [firstname, setFirstname] = useState("")
+    const [lastname, setLastname] = useState("")
+    const [studentID, setStudentID] = useState("")
+    const [faculty, setFaculty] = useState("")
+    const [year, setYear] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [repassword, setRepassword] = useState("")
+
+    useEffect(() => {
+        const fetchInfo = async () => {
+            let info = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/student/read/${userLineID}`)
+            setLineID(info.data.line_id)
+            setTitle(info.data.title)
+            setFirstname(info.data.firstname)
+            setLastname(info.data.lastname)
+            setStudentID(info.data.id)
+            setFaculty(info.data.faculty)
+            setYear(info.data.year)
+            setEmail(info.data.email)
+            setPassword(info.data.password)
+        }
+        fetchInfo()
+    })
+
+    const homepage = () => {
+        history.push("/")
+    }
+
+    const updateInfo = (e) => {
+        e.preventDefault()
+
+
+    }
+
+    const onChangeTitle = (e) => {
+        const selectedTitle = e.target.value
+        setTitle(selectedTitle)
+    }
+
     return (
-        <div>
-            Edit Info
+        <div className="App">
+            <div className="container background-header">
+                <div className="row">
+                    <div className="col-12 col-sm-12">
+                        <img src={Logo} alt="logo" width="144px" height="144px" />
+                    </div>
+                </div>
+                <div className="row" style={{ paddingBottom: "15px", fontSize: "24px", color: "#FFFFFF" }}>
+                    <div className="col-12 col-sm-12">
+                        Edit Info
+                    </div>
+                </div>
+            </div>
+            <div className="Editinfo-header">
+                <div className="container-fluid">
+                    <div className="row p-2">
+                        <div className="col-12">
+                            <span style={{ color: '#9E9E9E', fontWeight: 'bold' }}>
+                                Please enter details to edit info
+                            </span>
+                        </div>
+                    </div>
+                    <div className="row p-2">
+                        <div className="col-12 form-group">
+                            <input disabled className="form-control" placeholder="LINE ID" name="lineid" value={lineID} />
+                        </div>
+                    </div>
+                    <div className="row p-2">
+                        <div className="col-4">
+                            <select className="form-control" id="sel1" defaultValue={title} onChange={(e) => onChangeTitle(e)} >
+                                <option value="Title...">Select...</option>
+                                <option value="Mr.">Mr.</option>
+                                <option value="Mrs.">Mrs.</option>
+                                <option value="Miss">Miss</option>
+                            </select>
+                        </div>
+                        <div className="col-4">
+                            <input className="form-control" placeholder="Firstname" name="firstname" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+                        </div>
+                        <div className="col-4">
+                            <input className="form-control" placeholder="Lastname" name="lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="row p-2">
+                        <div className="col-6">
+                            <input className="form-control" placeholder="Student ID" name="studentid" value={studentID} onChange={(e) => setStudentID(e.target.value)} />
+                        </div>
+                        <div className="col-6">
+                            <input className="form-control" placeholder="Faculty" name="faculty" value={faculty} onChange={(e) => setFaculty(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="row p-2">
+                        <div className="col-6">
+                            <input className="form-control" placeholder="Year" name="year" value={year} onChange={(e) => setYear(e.target.value)} />
+                        </div>
+                        <div className="col-6">
+                            <input className="form-control" placeholder="Email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="row p-2">
+                        <div className="col-12 form-group">
+                            <input type="password" className="form-control" placeholder="Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="row p-2">
+                        <div className="col-12 form-group">
+                            <input type="password" className="form-control" placeholder="Re-Password" name="repassword" value={repassword} onChange={(e) => setRepassword(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="row p-2 d-flex justify-content-center">
+                        <div className="col-4">
+                            <button className="btn" onClick={(e) => updateInfo(e)} style={{ fontWeight: 'bold', color: '#FFFFFF', backgroundColor: '#91E45E', width: '100%' }}>Update</button>
+                        </div>
+                    </div>
+                    <div className="row p-2 d-flex justify-content-center">
+                        <div className="col-4">
+                            <button className="btn" onClick={homepage} style={{ fontWeight: 'bold', color: '#FFFFFF', backgroundColor: '#8199EF', width: '100%' }}>Home</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

29a5bd8aaeead7f0fc931ac002e4e7a198c374cd
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-10-21 22:26:57 +0700

    [EDIT] debug #1

--- src/components/CheckScoreResult.js
+++ src/components/CheckScoreResult.js
@@ -13,36 +13,38 @@ export default function CheckScore(props) {
     useEffect(() => {
         const fetchScore = async () => {
             let subject = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/score/subject/reads/${studentid}/${semester}/${subjectid}/${password}`)
+            console.log(subject.data)
             let activities = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/score/activity/reads/${semester}/${subject.data.id}/${subject.data.sectionid}`)
+            console.log(activities.data)
             let arrayActivity = Array(activities.data)
             console.log(arrayActivity)
-            arrayActivity.forEach((activity) => {
-                if (activity.activitytype === "Individual") {
-                    const fetchIndividual = async () => {
-                        let score = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/score/individual/read/${studentid}/${semester}/${subject.data.id}/${subject.data.sectionid}/${activity.activityid}`)
-                        scores.push(score.data)
-                    }
-                    fetchIndividual()
-                }
-                if (activity.activitytype === "Group") {
-                    const fetchGroup = async () => {
-                        let groups = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/score/groupid/reads/${semester}/${subject.data.id}/${subject.data.sectionid}/${activity.activityid}`)
-                        let arrayGroups = Array(groups.data)
-                        console.log(arrayGroups)
-                        arrayGroups.forEach((group) => {
-                            const fetchScore2 = async () => {
-                                let score = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/score/group/read/${studentid}/${semester}/${subject.data.id}/${subject.data.sectionid}/${activity.activityid}/${group.id}`)
-                                scores.push(score.data)
-                            }
-                            fetchScore2()
-                        })
-                    }
-                    fetchGroup()
-                }
-            })
+            // arrayActivity.forEach((activity) => {
+            //     if (activity.activitytype === "Individual") {
+            //         const fetchIndividual = async () => {
+            //             let score = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/score/individual/read/${studentid}/${semester}/${subject.data.id}/${subject.data.sectionid}/${activity.activityid}`)
+            //             scores.push(score.data)
+            //         }
+            //         fetchIndividual()
+            //     }
+            //     if (activity.activitytype === "Group") {
+            //         const fetchGroup = async () => {
+            //             let groups = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/score/groupid/reads/${semester}/${subject.data.id}/${subject.data.sectionid}/${activity.activityid}`)
+            //             let arrayGroups = Array(groups.data)
+            //             console.log(arrayGroups)
+            //             arrayGroups.forEach((group) => {
+            //                 const fetchScore2 = async () => {
+            //                     let score = await axios.get(`https://us-central1-sit-scor-b4c38.cloudfunctions.net/app/api/liff/score/group/read/${studentid}/${semester}/${subject.data.id}/${subject.data.sectionid}/${activity.activityid}/${group.id}`)
+            //                     scores.push(score.data)
+            //                 }
+            //                 fetchScore2()
+            //             })
+            //         }
+            //         fetchGroup()
+            //     }
+            // })
         }
         fetchScore()
-    }, [studentid, semester, subjectid, password, scores])
+    }, [studentid, semester, subjectid, password])
 
     return (
         <div>

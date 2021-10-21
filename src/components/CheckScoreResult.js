70d84a87ce88acc55855cf39342b735163eab7d9
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-10-21 21:10:18 +0700

    [EDIT] show data that pass by props

--- src/components/CheckScoreResult.js
+++ src/components/CheckScoreResult.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-export default function CheckScore() {
+export default function CheckScore(props) {
+    
+    const userLineID = props.location.state.userLineID;
+    const studentid = props.location.state.studentid;
+    const semester = props.location.state.semester;
+    const subjectid = props.location.state.subjectid;
+    const password = props.location.state.password;
+    const fetchPassword = props.location.state.fetchPassword;
+
     return (
         <div>
-            
+            {userLineID}<br />
+            {studentid}<br />
+            {semester}<br />
+            {subjectid}<br />
+            {password}<br />
+            {fetchPassword}<br />
         </div>
     )
 }

3ace9afcb2747029a9fa7f0cd0e4ca2876bd32d0
Supakrit Akkeedet <supakrit.kit@mail.kmutt.ac.th>
2021-11-01 16:17:35 +0700

    [EDIT] delete #5

--- src/components/Verify.js
+++ src/components/Verify.js
@@ -38,8 +38,8 @@ export default function Verify(props) {
         const { maxLength, value, name } = e.target;
         const [fieldName, fieldIndex] = name.split("-");
 
-        if (value.length === maxLength) {
-            if (parseInt(fieldIndex, 10) < 4) {
+        if (value.length >= maxLength) {
+            if (parseInt(fieldIndex, 10) <= 4) {
                 const nextSibling = document.querySelector(
                     `input[name=number-${parseInt(fieldIndex, 10) + 1}]`
                 );
